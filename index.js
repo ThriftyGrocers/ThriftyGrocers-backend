4ed92f0764766544e1ce405b18e26806feaa66e3
aqilf <afaizala@students.cs.ubc.ca>
2024-01-20 20:51:27 -0800

    Routes updated

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// require('dotenv').config();
+
+// const mongoString = process.env.DATABASE_URL
+// console.log(mongoString);
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
@@ -5,6 +9,7 @@ const routes = require('./routes/routes');
 
 
 const app = express();
+//express is also capable of json parsing
 app.use(express.json());
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`);
--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,36 @@ const express = require('express');
 
 const router = express.Router()
 
+
+//These methods fulfill the post, get, patch and delete requirements of REST APIs
+//Req is the recieving requests from our client
+//Res is the response to our client
+//Client is our front end user
+
+//Post Method
+router.post('/post', (req, res) => {
+    res.send('Post API')
+})
+
+//Get all Method
+router.get('/getAll', (req, res) => {
+    res.send('Get All API')
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+
 module.exports = router;
