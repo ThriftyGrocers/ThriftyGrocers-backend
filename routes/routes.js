f8f307450e1cf13292afc59d9748f0db371b56a5
aqilf <afaizala@students.cs.ubc.ca>
2024-01-22 17:41:04 -0800

    Updated get all, get by id, update by id and delete by id.

--- routes/routes.js
+++ routes/routes.js
@@ -10,29 +10,54 @@ const router = express.Router()
 //Res is the response to our client
 //Client is our front end user
 
-//Post Method
-// router.post('/post', (req, res) => {
-//     res.send('Post API')
-// })
-
 //Get all Method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/getAll', async (req, res) => {
+        try {
+            const data = await Model.find();
+            res.json(data)
+        } catch (error) {
+            res.status(500).json({message: error.message})
+        }
 })
 
 //Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/getOne/:id', async(req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 //Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = {new: true};
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 // post using the model
